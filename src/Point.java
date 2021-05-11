cd019a0ad24cad0abbb0268fe4fe6899649a33ce
Cezary Sanecki <devcezz@gmail.com>
2021-05-11 18:42:52 +0200

    Added twelfth classes in project

---
+++ src/Point.java
@@ -0,0 +1,15 @@
+public class Point {
+
+    final int x;
+    final int y;
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ src/Section.java
@@ -0,0 +1,17 @@
+public class Section {
+
+    final Point p1;
+    final Point p2;
+
+    public Section(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public double length() {
+        int deltaX = p2.x - p1.x;
+        int deltaY = p2.y - p1.y;
+        // √((x2 - x1)^2 + (y2 - y1)^2)
+        return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+    }
+}
